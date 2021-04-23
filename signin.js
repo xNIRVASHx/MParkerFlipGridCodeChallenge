5a3433eab3253abb7ed1f72cadf53def0b6df70a
xNIRVASHx <pmyle@NIRVASH>
2021-04-23 10:18:11 -0400

    init

--- signin.js
+++ signin.js
@@ -1,6 +1,48 @@
-function ab(){
+const password = document.getElementById("Password")
+const form = document.getElementById("Form")
+const errorElement = document.getElementById("error")
+form.addEventListener("submit", (e) => {
+    let messages = []
 
-    event.preventDefault();/*prevent page from reloading */
+    if(password.value.length <=8){
+        messages.push("Password must be longer than 8 characters")
+    }
+
+    if(password.value.length >= 20){
+        messages.push("Password must be shorter than 20 characters")
+    }
+
+    if(password.value === "password"){
+        messages.push("Password cannot be password")
+    }
+
+
+
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(", ")
+    }
+
+    if(messages.length < 1){
+        event.preventDefault();/*prevent page from reloading */
+        document.getElementById('instructions').innerText = "You have been registered for this awesome service. Please check your email listed below for instructions"; //replace paragraph
+        
+        document.getElementById('btn').innerText = "Sign In";
+        
+        const userName = document.getElementById("FirstName").value;
+        document.getElementById("header").innerHTML = "Welcome<br>" + userName;
+    
+        var yah = document.getElementById("Email").value;
+        document.getElementById("displayEmail").innerHTML = yah;
+        document.getElementById("list").remove();
+        document.getElementById("error").remove();
+
+    }
+})
+
+/*function ab(){
+
+    event.preventDefault();/*prevent page from reloading *//*
     document.getElementById('instructions').innerText = "You have been registered for this awesome service. Please check your email listed below for instructions"; //replace paragraph
     
     document.getElementById('btn').innerText = "Sign In";
@@ -13,4 +55,4 @@ function ab(){
     document.getElementById("list").remove();
 
 
-}
+}*/
