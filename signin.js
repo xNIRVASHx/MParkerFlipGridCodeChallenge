38261d76606aaa0afeb0dd90ec02bdee3538a5b5
xNIRVASHx <pmyle@NIRVASH>
2021-04-23 09:39:26 -0400

    init

--- signin.js
+++ signin.js
@@ -1,14 +1,16 @@
 function ab(){
 
     event.preventDefault();/*prevent page from reloading */
-    document.getElementById('help').innerText = "You have been registered for this awesome service. Please check your email listed below for instructions"; //replace paragraph
+    document.getElementById('instructions').innerText = "You have been registered for this awesome service. Please check your email listed below for instructions"; //replace paragraph
     
     document.getElementById('btn').innerText = "Sign In";
     
-    var yah = document.getElementById("Email").value;
-    document.getElementById("ee").innerHTML = yah;
-    document.getElementById("dac").remove();
-
-    var userName = document.getElementById("FirstName").value;
+    const userName = document.getElementById("FirstName").value;
     document.getElementById("header").innerHTML = "Welcome<br>" + userName;
+
+    var yah = document.getElementById("Email").value;
+    document.getElementById("displayEmail").innerHTML = yah;
+    document.getElementById("list").remove();
+
+
 }
